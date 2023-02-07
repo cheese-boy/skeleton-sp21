77abd8ae80374bc493575f938d11a9b8aed30f66
vadimchiriac <117806780+vadimchiriac@users.noreply.github.com>
2023-02-07 13:29:36 +0200

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,25 +3,27 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n /2;
         } else {
-            return n * 2;
+            return 3*n + 1;
         }
+
     }
 
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
+
+        // Some starter code to test
+        while (n != 1) {          
+            n = nextNumber(n);          
             System.out.print(n + " ");
         }
         System.out.println();
+
     }
 }
 
